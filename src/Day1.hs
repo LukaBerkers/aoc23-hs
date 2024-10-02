84d44d3c46a853dbd27f3b8239e0fd10f91c7793
Luka <luka@berkers.dev>
2024-10-02 10:49:17 +0200

    docs: add documentation for `recoverCalibrationValues`, `inputDir`, and `readInputFile`
    Added haddocks for better understanding of the functions and constants.

--- src/Day1.hs
+++ src/Day1.hs
@@ -9,9 +9,17 @@ import System.FilePath ((</>))
 calibrationDocumentFileName :: FilePath
 calibrationDocumentFileName = inputDir </> "day1.txt"
 
+-- | Reads the calibration document and returns the lines as a list of strings.
 readCalibrationDocument :: IO [String]
 readCalibrationDocument = readInputFile calibrationDocumentFileName id
 
+{- | Recovers the calibration values from the calibration document lines.
+
+The calibration values are the first and last digit of each line in the calibration document.
+There may be only one digit in a line, in which case the first and last digit are the same.
+If any line does not contain a calibration value, 'Nothing' is returned.
+Otherwise, the calibration values are returned as a list of integers.
+-}
 recoverCalibrationValues :: [String] -> Maybe [Int]
 recoverCalibrationValues = traverse recoverCalibrationValue
 
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,7 +1,9 @@
 module Utils (readInputFile, inputDir) where
 
+-- | The directory where the input files are stored.
 inputDir :: FilePath
 inputDir = "input"
 
+-- | Reads a file line by line and parses each line using the provided parser.
 readInputFile :: FilePath -> (String -> a) -> IO [a]
 readInputFile filePath lineParser = map lineParser . lines <$> readFile filePath

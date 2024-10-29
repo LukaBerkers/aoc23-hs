364317bb0077de820246ce6eeb949935335ca39b
Luka <luka@berkers.dev>
2024-10-29 15:48:11 +0100

    feat: expose `CubeSet` constructor and fields

--- src/Day2.hs
+++ src/Day2.hs
@@ -19,7 +19,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 module Day2 (
     -- * Data types
     Game,
-    CubeSet,
+    CubeSet (..),
     InputError (..),
 
     -- * File reading
